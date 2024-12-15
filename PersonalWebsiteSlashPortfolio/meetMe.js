c78fd5c5e5538601d2195c76d48db25038d82994
eSteffen26 <easton.steffen5875@gmail.com>
2024-12-15 13:54:10 -0600

    Look I made progress!

---
+++ PersonalWebsiteSlashPortfolio/meetMe.js
@@ -0,0 +1,458 @@
+
+let box1 =document.querySelector("#box1")
+
+function box1Function()
+{
+    let box =document.querySelector("#box1")
+    box.innerText = "I watched the original movies when I was a kid and fell in love. "
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box1BackToNormal);
+
+}
+
+function box1BackToNormal()
+{
+    let box =document.querySelector("#box1")
+    box.innerText = "I love Teenage Mutant Ninja Turtles"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box2Function()
+{
+    let box =document.querySelector("#box2")
+    box.innerText = "For my job with the UNI Chemistry Department, I had to follow a research team into caves and film them."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box2BackToNormal);
+
+}
+
+function box2BackToNormal()
+{
+    let box =document.querySelector("#box2")
+    box.innerText = "Spent over 100 hours in caves"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box3Function()
+{
+    let box =document.querySelector("#box3")
+    box.innerText = "I did FLL from 4th to 7th grade, FTC from 7th to 9th grade, and FRC from 9th to 12th grade."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box3BackToNormal);
+
+}
+
+function box3BackToNormal()
+{
+    let box =document.querySelector("#box3")
+    box.innerText = "Did 3 levels of FIRST Robotics (FLL, FTC, and FRC)"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box4Function()
+{
+    let box =document.querySelector("#box4")
+    box.innerText = "I have lived in Cedar Falls for 16 years."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box4BackToNormal);
+
+}
+
+function box4BackToNormal()
+{
+    let box =document.querySelector("#box4")
+    box.innerText = "Grew up in Cedar Falls"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box5Function()
+{
+    let box =document.querySelector("#box5")
+    box.innerText = "Canada with my family in 2017. Italy with the UNI Cecilians in May 2024. London with the UNI PMB in December 2024/January 2025"
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box5BackToNormal);
+
+}
+
+function box5BackToNormal()
+{
+    let box =document.querySelector("#box5")
+    box.innerText = "Have traveled out of the US to Canada, Italy, and England"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box6Function()
+{
+    let box =document.querySelector("#box6")
+    box.innerText = "I have tried to teach myself how to animate on and off for years."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box6BackToNormal);
+
+}
+
+function box6BackToNormal()
+{
+    let box =document.querySelector("#box6")
+    box.innerText = "Trying to learn how to animate"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box7Function()
+{
+    let box =document.querySelector("#box7")
+    box.innerText = "When I was un upper elementary and middle school I played both hockey and soccer. I love them both."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box7BackToNormal);
+
+}
+
+function box7BackToNormal()
+{
+    let box =document.querySelector("#box7")
+    box.innerText = "Used to play hockey and soccer"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box8Function()
+{
+    let box =document.querySelector("#box8")
+    box.innerText = "I have been slowly teaching myself piano for years. Not great yet but getting there."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box8BackToNormal);
+
+}
+
+function box8BackToNormal()
+{
+    let box =document.querySelector("#box8")
+    box.innerText = "Currently teaching myself piano"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box9Function()
+{
+    let box =document.querySelector("#box9")
+    box.innerText = "I have over 200 hours in the game and have completed 100% of it. I think I might like it a little."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box9BackToNormal);
+
+}
+
+function box9BackToNormal()
+{
+    let box =document.querySelector("#box9")
+    box.innerText = "I love Legend of Zelda Breath of the Wild"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box10Function()
+{
+    let box =document.querySelector("#box10")
+    box.innerText = "I am non-binary, asexual, and aromantic."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box10BackToNormal);
+
+}
+
+function box10BackToNormal()
+{
+    let box =document.querySelector("#box10")
+    box.innerText = "Part of the LGBTQ+ community"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box11Function()
+{
+    let box =document.querySelector("#box11")
+    box.innerText = "I am in the PMB in the Baritone Tenor Sax(BTS) section. I am a marching section leader."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box11BackToNormal);
+
+}
+
+function box11BackToNormal()
+{
+    let box =document.querySelector("#box11")
+    box.innerText = "In the UNI Panther Marching Band"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box12Function()
+{
+    let box =document.querySelector("#box12")
+    box.innerText = "I make art with all kinds of mediums. From pencil to clay to paint to digital. I love it all."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box12BackToNormal);
+
+}
+
+function box12BackToNormal()
+{
+    let box =document.querySelector("#box12")
+    box.innerText = "I love art and making it"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box13Function()
+{
+    let box =document.querySelector("#box13")
+    box.innerText = "I have a younger sister and brother. My sister is 2 years younger and my brother is 4 years younger. I love them both a lot."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box13BackToNormal);
+
+}
+
+function box13BackToNormal()
+{
+    let box =document.querySelector("#box13")
+    box.innerText = "I have 2 younger siblings"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box14Function()
+{
+    let box =document.querySelector("#box14")
+    box.innerText = "My dog's names are Ginger(who looks so sweet but it secretly the worst) and Garvey(who is a huge people pleaser with nothing in his head)."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box14BackToNormal);
+
+}
+
+function box14BackToNormal()
+{
+    let box =document.querySelector("#box14")
+    box.innerText = "I have 2 dogs"
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box15Function()
+{
+    let box =document.querySelector("#box15")
+    box.innerText = "I was born in Missouri despite crowing up in Iowa."
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box15BackToNormal);
+
+}
+
+function box15BackToNormal()
+{
+    let box =document.querySelector("#box15")
+    box.innerText = "Born in Missouri"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box16Function()
+{
+    let box =document.querySelector("#box16")
+    box.innerText = "I started playing Minecraft with my siblings and have continued. One of my favorite games ever."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box16BackToNormal);
+
+}
+
+function box16BackToNormal()
+{
+    let box =document.querySelector("#box16")
+    box.innerText = "I love Minecraft"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box17Function()
+{
+    let box =document.querySelector("#box17")
+    box.innerText = "I love being active in anyway. From sports to workout I love it all."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box17BackToNormal);
+
+}
+
+function box17BackToNormal()
+{
+    let box =document.querySelector("#box17")
+    box.innerText = "I love to be active"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box18Function()
+{
+    let box =document.querySelector("#box18")
+    box.innerText = "My child(tortoise)'s name is Red and I love them a lot. I've had them for 8 years now."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box18BackToNormal);
+
+}
+
+function box18BackToNormal()
+{
+    let box =document.querySelector("#box18")
+    box.innerText = "I have a tortoise"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box19Function()
+{
+    let box =document.querySelector("#box19")
+    box.innerText = "I have always had a love for legos. It is what got me into robotics in the first place."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box19BackToNormal);
+
+}
+
+function box19BackToNormal()
+{
+    let box =document.querySelector("#box19")
+    box.innerText = "I love Lego"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box20Function()
+{
+    let box =document.querySelector("#box20")
+    box.innerText = "I have several Sonic videos games, Frontiers, Mania, Forces and more. I've enjoyed each one even when their faults show."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box20BackToNormal);
+
+}
+
+function box20BackToNormal()
+{
+    let box =document.querySelector("#box20")
+    box.innerText = "I love Sonic the Hedgehog"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box21Function()
+{
+    let box =document.querySelector("#box21")
+    box.innerText = "From 10th to 12th grade I helped backstage for 5 shows. Some of the most fun I've had."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box21BackToNormal);
+
+}
+
+function box21BackToNormal()
+{
+    let box =document.querySelector("#box21")
+    box.innerText = "I was apart of my high schools theater tech crew"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box22Function()
+{
+    let box =document.querySelector("#box22")
+    box.innerText = "I have 20 different succulents in my room sitting on my window sill. I also have a few non-succulent plants"    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box22BackToNormal);
+
+}
+
+function box22BackToNormal()
+{
+    let box =document.querySelector("#box22")
+    box.innerText = "I have a lot of plants"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box23Function()
+{
+    let box =document.querySelector("#box23")
+    box.innerText = "I make short little videos in my free time."    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box23BackToNormal);
+
+}
+
+function box23BackToNormal()
+{
+    let box =document.querySelector("#box23")
+    box.innerText = "I love making videos"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
+
+function box24Function()
+{
+    let box =document.querySelector("#box24")
+    box.innerText = "I started on trumpet then in middle school moved to baritone where I've been ever since"    
+    box.style.color = "#0a0a0a"
+    box.style.backgroundColor = "#fafafa"
+    box.onmouseout = function(){};
+    box.addEventListener("mouseout", box24BackToNormal);
+
+}
+
+function box24BackToNormal()
+{
+    let box =document.querySelector("#box24")
+    box.innerText = "I play the Baritone/Euphonium"    
+    box.style.color = "#fafafa"
+    box.style.backgroundColor = "#0a0a0a"
+}
